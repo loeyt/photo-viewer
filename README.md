1d877d974d769091ce52e009bcd69ce0b751d91f
Luit van Drongelen <luitvd@gmail.com>
2020-04-02 18:31:13 +0200

    initial commit, mostly from npx degit sveltejs/template moved around a little

---
+++ README.md
@@ -0,0 +1,4 @@
+# photo-viewer
+
+This is an attempt at building a Hugo module that supplies a Svelte photo
+viewer for a site I'm building.
---
+++ rollup.config.js
@@ -0,0 +1,35 @@
+import svelte from 'rollup-plugin-svelte'
+import resolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import { terser } from 'rollup-plugin-terser'
+
+const production = !process.env.ROLLUP_WATCH
+
+export default {
+  input: 'src/main.js',
+  output: {
+    sourcemap: true,
+    format: 'iife',
+    name: 'app',
+    file: 'assets/photo-viewer/bundle.js'
+  },
+  plugins: [
+    svelte({
+      dev: !production,
+      css: css => {
+        css.write('assets/photo-viewer/bundle.css')
+      }
+    }),
+
+    resolve({
+      browser: true,
+      dedupe: ['svelte']
+    }),
+    commonjs(),
+
+    production && terser()
+  ],
+  watch: {
+    clearScreen: false
+  }
+}
