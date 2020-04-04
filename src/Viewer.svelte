6fc8a69b65e84dbd7693ceb3071238fe2555a9b1
Luit van Drongelen <luitvd@gmail.com>
2020-04-05 00:23:14 +0200

    change navigation behavior

--- src/Viewer.svelte
+++ src/Viewer.svelte
@@ -4,14 +4,37 @@
   import Image from "./Image.svelte";
   export let dataURL;
 
-  let imageData = fetch(dataURL).then(response => response.json());
+  let imageData = (async dataURL => {
+    const response = await fetch(dataURL);
+    const data = await response.json();
+
+    // Now we know which links to highjack.
+    for (const t of document.links) {
+      const href = t.getAttribute("href");
+      if (href in data) {
+        t.addEventListener("click", event => {
+          event.preventDefault();
+          if (window.location.hash) {
+            // Navigation from image to image is not preserved in history.
+            history.replaceState({}, "", href);
+          } else {
+            history.pushState({}, "", href);
+          }
+          // These history invocations don't trigger hashchange, so we do here.
+          selected = window.location.hash;
+        });
+      }
+    }
+    return data;
+  })(dataURL);
 
   let selected = window.location.hash;
 
   function keydown(event) {
     if (event.key == "Escape") {
-      history.replaceState({}, "", window.location.pathname);
-      hashchange();
+      if (window.location.hash) {
+        history.back();
+      }
     }
 
     // TODO: ArrowLeft + ArrowDown for previous
@@ -19,10 +42,6 @@
   }
 
   // TODO: click listener for click on background
-
-  function hashchange(event) {
-    selected = window.location.hash;
-  }
 </script>
 
 <style>
@@ -40,16 +59,18 @@
   }
 </style>
 
-<svelte:window on:hashchange={hashchange} on:keydown={keydown} />
+<svelte:window
+  on:hashchange={() => {
+    selected = window.location.hash;
+  }}
+  on:keydown={keydown} />
 
 {#if selected}
   {#await imageData then data}
-    {#if data[selected]}
-      <div transition:fade>
-        <div class="current">
-          <Image alt={selected} data={data[selected]} />
-        </div>
+    <div transition:fade>
+      <div class="current">
+        <Image alt={selected} data={data[selected]} />
       </div>
-    {/if}
+    </div>
   {/await}
 {/if}
